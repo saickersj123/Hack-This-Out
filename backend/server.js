76ecfe8319a8edff6bfd70a9314c0fe8a3f8f70a
Flynn <150012248+saickersj123@users.noreply.github.com>
2024-10-05 15:36:01 +0900

    update origin & api baseURL

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://app.hackthisout.o-r.kr/"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
