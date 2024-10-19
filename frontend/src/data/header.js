2714c3ada03cf9bf6fd512d67ac755897742843c
Flynn <150012248+saickersj123@users.noreply.github.com>
2024-10-19 20:45:33 +0900

    Revert "Update header.js"
    This reverts commit 030987f5785e3f136411136d2a90d74c2630ba42.

--- frontend/src/data/header.js
+++ frontend/src/data/header.js
@@ -35,11 +35,6 @@ export const headerMenus = [
         title: "Instances",
         icon: <GrCloudComputer />,
         src: "/Instances"
-    },
-    {
-        title: "Machine Management",
-        icon: <GrCloudComputer />,
-        src: "/MachineManagement"
     }
 ];
 
