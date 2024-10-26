4a6c4644b6bb0086fb6273285357e6e11a6c47d9
Flynn <150012248+saickersj123@users.noreply.github.com>
2024-10-26 11:57:15 +0900

    machine list

--- frontend/src/pages/machine/MachineListPage.jsx
+++ frontend/src/pages/machine/MachineListPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import MachineList from '../../components/machine/MachineList';
 import '../../assets/scss/machine/MachineListPage.scss';
+import Main from '../../components/main/Main';
 const MachineListPage = () => {
     
     return (
-        <div className= 'machine-list-page'>
-            <MachineList />
-        </div>
+        <Main>
+            <div className= 'machine-list-page'>
+                <MachineList />
+            </div>
+        </Main>
     );
 };
 
