722ec822ba9f423aaa2993fdba7eed2060e68d3a
Flynn <150012248+saickersj123@users.noreply.github.com>
2024-10-30 00:30:47 +0900

    hotfix

--- frontend/src/components/contest/AddContestForm.jsx
+++ frontend/src/components/contest/AddContestForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { createContest, getAllMachines } from '../../api/axiosInstance';
 import '../../assets/scss/contest/AddContestForm.scss';
 
