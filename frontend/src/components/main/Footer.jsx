f72cc3e4e7c89e00e14af1e3bde820c370120a2f
Flynn <150012248+saickersj123@users.noreply.github.com>
2024-11-02 18:45:06 +0900

    env updates

--- frontend/src/components/main/Footer.jsx
+++ frontend/src/components/main/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer id='footer' role="contentinfo"> 
-            <a href="https://github.com/saickersj123/Hack-This-Out">Hack This Out</a>
+            <a href="https://github.com/saickersj123/Hack-This-Out"><p>&copy; Hack This Out 2024</p></a>
         </footer>
     )
 }
