9c2c0b3567b0c991e4adc96c632c07d4ba2e3330
err0r785 <pjw4173121@gmail.com>
2024-11-12 17:23:07 +0900

    Update MachinDetail

--- frontend/src/components/machine/MachineDetail.tsx
+++ frontend/src/components/machine/MachineDetail.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MachineDetail as MachineDetailType } from '../../types/Machine';
 import '../../assets/scss/machine/MachineDetail.scss';
+import StarRatings from 'react-star-ratings';
 
 /**
  * Props interface for MachineDetail component.
@@ -20,15 +21,31 @@ const MachineDetail: React.FC<MachineDetailProps> = ({ machineDetail }) => {
 
   return (
     <div className="machine-detail-container">
+      <div className="machine-detail-header">
+        <h3 className="machine-name">{name}</h3>
+        <div className="machine-rating">
+          <StarRatings
+            rating={rating}
+            starRatedColor="orange"
+            numberOfStars={5}
+            name='rating'
+            starDimension="20px"
+            starSpacing="3px"
+          />
+          <p className="rating-text">Rating: {rating.toFixed(1) || 'N/A'}</p>
+        </div>
+      </div>
+      <hr />
       <div className="machine-detail-content">
-        <h3>Machine Details</h3>
-        <p><strong>Name:</strong> {name || 'N/A'}</p>
-        <p><strong>Category:</strong> {category || 'N/A'}</p>
-        <p><strong>Rating:</strong> {rating.toFixed(1) || 'N/A'}</p>
-        <p><strong>Description:</strong> {description || 'N/A'}</p>
-        <p><strong>Experience Points (EXP):</strong> {exp || 0}</p>
-        <p><strong>AMI ID:</strong> {amiId || 'N/A'}</p>
-        {/* Add more fields as necessary */}
+        <div className="left-section">
+          <p><strong>About name</strong></p>
+          <p>{description || 'N/A'}</p>
+        </div>
+        <div className="right-section">
+          <p><strong>Category:</strong> {category || 'N/A'}</p>
+          <p><strong>Experience Points (EXP):</strong> {exp || 0}</p>
+          <p><strong>AMI ID:</strong> {amiId || 'N/A'}</p>
+        </div>
       </div>
     </div>
   );
