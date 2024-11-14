3baf99868508b8e81cd9f3f62a057623d74a3c74
BlackMuji <kmi00203@gmail.com>
2024-11-14 18:10:15 +0900

    Sidebar update

--- frontend/src/contexts/SidebarContext.tsx
+++ frontend/src/contexts/SidebarContext.tsx
@@ -1,9 +1,6 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 interface SidebarContextProps {
-  selectedItem: string;
-  setSelectedItem: (item: string) => void;
-
   isCollapsed: boolean;
   toggleSidebar: () => void;
 }
@@ -11,16 +8,10 @@ interface SidebarContextProps {
 const SidebarContext = createContext<SidebarContextProps | undefined>(undefined);
 
 export const SidebarProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  // localStorage에서 초기 상태를 불러와 상태 저장
+  // localStorage에서 초기 상태 불러오기
   const [isCollapsed, setIsCollapsed] = useState(() => {
     const storedState = localStorage.getItem('isSidebarCollapsed');
     return storedState === 'true';
-  });
-
-  // selectedItem 상태 추가
-  const [selectedItem, setSelectedItem] = useState<string>(() => {
-    const storedItem = localStorage.getItem('selectedItem');
-    return storedItem || ''; // selectedItem을 localStorage에서 가져옴
   });
 
   const toggleSidebar = () => {
@@ -31,15 +22,13 @@ export const SidebarProvider: React.FC<{ children: React.ReactNode }> = ({ child
     });
   };
 
-  // selectedItem 상태를 localStorage에 저장
-  useEffect(() => {
-    if (selectedItem) {
-      localStorage.setItem('selectedItem', selectedItem);
-    }
-  }, [selectedItem]);
-
   return (
-    <SidebarContext.Provider value={{ selectedItem, setSelectedItem, isCollapsed, toggleSidebar }}>
+    <SidebarContext.Provider
+      value={{
+        isCollapsed,
+        toggleSidebar,
+      }}
+    >
       {children}
     </SidebarContext.Provider>
   );
