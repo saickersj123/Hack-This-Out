e6cdf0f49349cc2db17ce5d5f25ee7f32fde64b1
Flynn <150012248+saickersj123@users.noreply.github.com>
2024-11-16 17:48:05 +0900

    chunck rollup fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -27,5 +27,14 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output:{
+          manualChunks(id) {
+              if (id.includes('node_modules')) {
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+          }
+      }
+  }
   },
 });
