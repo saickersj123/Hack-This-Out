cd89a77dc8dea236fe8561469432dd90345a365e
err0r785 <pjw4173121@gmail.com>
2024-11-16 17:13:13 +0900

    Update MachineListPage

--- frontend/src/components/machine/MachineList.tsx
+++ frontend/src/components/machine/MachineList.tsx
@@ -86,91 +86,87 @@ const MachineList: React.FC = () => {
 
   return (
     <div className='machine-list-container'>
-      <div className='machine-list-title'>
-        <h2>Machine List</h2>
-      </div>
-      <div className="table-form">
-        <table className='machine-list-table'>
-          <thead>
-            <tr className='table-head'>
-              <th className='table-image'></th>
-              <th className='table-name'>Machine Name</th>
-              <th className='table-category'>Category
-                <ClickAwayListener onClickAway={handleClickAway}>
-                  <div className='category-filter-toggle'>
-                    <FilterAltIcon onClick={toggleFilterVisibility} />
-                    {filterVisible && (
-                      <div className='category-filter'>
-                        <label htmlFor='category-select' style={{ color: "black" }}>Filter by Category: </label>
-                        <select
-                          id='category-select'
-                          value={categoryFilter}
-                          onChange={(e) => setCategoryFilter(e.target.value)}
-                          style={{ border: "solid" }}
-                        >
-                          <option value=''>All</option>
-                          {categories.map((category) => (
-                            <option key={category} value={category}>{category}</option>
-                          ))}
-                        </select>
-                      </div>
-                    )}
-                  </div>
-                </ClickAwayListener>
-              </th>
-              <th className='table-rating'>Rating</th>
-              <th className='table-playCount'>Played</th>
-              <th className='table-details'>Detail</th>
+      <div className='machine-list-title'>Machine List</div>
+      <table className='machine-list-table'>
+        <thead>
+          <tr className='table-head'>
+            <th className='table-image'></th>
+            <th className='table-name'>Name</th>
+            <th className='table-category'>
+              Category
+              <ClickAwayListener onClickAway={handleClickAway}>
+                <div className='category-filter-toggle'>
+                  <FilterAltIcon onClick={toggleFilterVisibility} />
+                  {filterVisible && (
+                    <div className='category-filter'>
+                      <label htmlFor='category-select' style={{ color: "black" }}>Filter by Category:</label>
+                      <select
+                        id='category-select'
+                        value={categoryFilter}
+                        onChange={(e) => setCategoryFilter(e.target.value)}
+                      >
+                        <option value=''>All</option>
+                        {categories.map((category) => (
+                          <option key={category} value={category}>{category}</option>
+                        ))}
+                      </select>
+                    </div>
+                  )}
+                </div>
+              </ClickAwayListener>
+            </th>
+            <th className='table-rating'>Rating</th>
+            <th className='table-playCount'>Played</th>
+            <th className='table-details'>Detail</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredMachines.length === 0 ? (
+            <tr>
+              <td colSpan={6} className="no-data">No machines available.</td>
             </tr>
-          </thead>
-          <tbody>
-            {filteredMachines.length === 0 ? (
-              <tr>
-                <td colSpan={6} className="no-data">No machines available.</td>
-              </tr>
-            ) : (
-              filteredMachines.map((machine) => {
-                const avatarColorIndex = getAvatarColorIndex(machine.name);
-                const avatarBgColor = avatarBackgroundColors[avatarColorIndex];
-                return (
-                  <tr key={machine._id}>
-                    <td className='machine-img'>
-                      <Avatar
-                        variant="rounded"
-                        sx={{ backgroundColor: avatarBgColor, width: 50, height: 50 }}
-                      >
-                        {machine.name.charAt(0).toUpperCase()}
-                      </Avatar>
-                    </td>
-                    <td className='machine-name'>{machine.name}</td>
-                    <td className='machine-category'>{machine.category}</td>
-                    <td className='machine-rating'>
-                      <Box
-                        sx={{
-                          display: 'flex',
-                          alignItems: 'center',
-                        }}
-                      >
-                        <Rating
-                          name={`read-only-rating-${machine._id}`}
-                          value={Number(machine.rating)}
-                          precision={0.5}
-                          readOnly
-                        />
-                      </Box>
-                    </td>
-                    <td className='machine-playCount'>{machine.playerCount}</td>
-                    <td className='machine-details'>
-                      <button className='details-button' onClick={() => handleMachineClick(machine)}><FaArrowRightToBracket size={24} /></button>
-                    </td>
-                  </tr>
-                );
-              })
-            )}
-          </tbody>
-        </table>
-      </div>
-    </div >
+          ) : (
+            filteredMachines.map((machine) => {
+              const avatarColorIndex = getAvatarColorIndex(machine.name);
+              const avatarBgColor = avatarBackgroundColors[avatarColorIndex];
+              return (
+                <tr key={machine._id}>
+                  <td className='machine-img'>
+                    <Avatar
+                      variant="rounded"
+                      sx={{ backgroundColor: avatarBgColor, width: 50, height: 50 }}
+                    >
+                      {machine.name.charAt(0).toUpperCase()}
+                    </Avatar>
+                  </td>
+                  <td className='machine-name'>{machine.name}</td>
+                  <td className='machine-category'>{machine.category}</td>
+                  <td className='machine-rating'>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <Rating
+                        name={`read-only-rating-${machine._id}`}
+                        value={Number(machine.rating)}
+                        precision={0.5}
+                        readOnly
+                      />
+                    </Box>
+                  </td>
+                  <td className='machine-playCount'>{machine.playerCount}</td>
+                  <td className='machine-details'>
+                    <button className='details-button' onClick={() => handleMachineClick(machine)}><FaArrowRightToBracket size={24} /></button>
+                  </td>
+                </tr>
+              );
+            })
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
